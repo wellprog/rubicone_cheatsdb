9342583fd6f6e2d122e491d632928676b27416d6
Velor <velorkhalilov@gmail.com>
2020-05-06 19:46:09 +0300

    fix

--- categories.php
+++ categories.php
@@ -1,4 +1,55 @@
-<?php include "components/header.php" ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Categories</title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="Sublime project">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="styles/bootstrap4/bootstrap.min.css">
+<link href="plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/owl.carousel.css">
+<link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/owl.theme.default.css">
+<link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/animate.css">
+<link rel="stylesheet" type="text/css" href="styles/categories.css">
+<link rel="stylesheet" type="text/css" href="styles/categories_responsive.css">
+</head>
+<body>
+
+<div class="super_container">
+
+	<!-- Header -->
+
+	<header class="header">
+		<div class="header_container">
+			<div class="container">
+				<div class="row">
+					<div class="col">
+						<div class="header_content d-flex flex-row align-items-center justify-content-start">
+							<div class="logo"><a href="#">CheatsDB</a></div>
+							<nav class="main_nav">
+								<ul>
+									<li class="hassubs active">
+										<a href="index.html">Главная</a>
+										<ul>
+											<li><a href="categories.html">Категории читов</a></li>
+											<li><a href="product.html">Читы</a></li>
+											<li><a href="cart.html">Корзина</a></li>
+											<li><a href="checkout.html">Покупка</a></li>
+											<li><a href="contact.html">Контакты</a></li>
+										</ul>
+									</li>
+									<li><a href="contact.html">Контакты</a></li>
+								</ul>
+							</nav>
+							
+								<div class="hamburger"><i class="fa fa-bars" aria-hidden="true"></i></div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 		
 		<!-- Search Panel -->
 		<div class="search_panel trans_300">
